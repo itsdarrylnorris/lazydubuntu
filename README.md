4f338c9d02c15d2ab4dd322b3ac87d37e2caa377
Darryl Norris <admin@darrylnorris.com>
2014-10-28 17:17:33 -0500

    Atom Packages for web development and Drupal, Git Configuration, Installation of SmartGit, Backup files, TODO List Markdown and README.md description.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Lazy Dubuntu
 ===========
 
-Setting Up my Drupal Enviroment in Ubuntu
+Setting Up my Drupal Environment in Ubuntu.
+
+The goal of this project is to be able to set-up a dev environment with one
+click. This script will run in your ubuntu and will set-up everything that you
+need to start developing website with Drupal and others technologies.
--- lazydubuntu.sh
+++ lazydubuntu.sh
@@ -16,6 +16,7 @@ sudo apt-get install lamp-server^ -y
 # to set up a password for the root user.
 
 # Install phpMyAdmin
+sudo apt-get update
 sudo apt-get install phpmyadmin -y
 
 # During the installation, phpMyAdmin will walk you through a basic
@@ -34,10 +35,22 @@ sudo apt-get install phpmyadmin -y
 # configuration. sudo nano /etc/apache2/apache2.conf
 # Add the phpmyadmin config to the file. Include /etc/phpmyadmin/apache.conf
 
+sudo mkdir ~/LazyDubuntu/originals/
+sudo cp /etc/phpmyadmin/apache.conf ~/LazyDubuntu/originals/
+
 sudo echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
 
 # Enable mode_rewrite for clean urls.
 sudo a2enmod rewrite
+
+# For single server configuration you want to do this.
+# Find <Directory /var/www/html> and replace it with <Directory /var/www/>
+# In file apache2.conf and 000-default.conf
+
+WWW='<Directory /var/www/html>'
+OLDSCHOOL='<Directory /var/www'
+cat textfile.txt | /bin/sed -i -e 's/$WWW/$OLDSCHOOL/g' ~/Desktop/textfile.txt
+
 
 # Modify Apache so it can rewrite.
 lineNumber="$( /bin/grep -n "/var/www/" /etc/apache2/apache2.conf | /usr/bin/head -1 | cut -d ":" -f 1 )"
@@ -50,6 +63,46 @@ sudo service apache2 restart
 # Install git
 sudo apt-get update
 sudo apt-get install git
+
+# Git Configuration
+# To learn more about git configuration:
+# http://git-scm.com/book/en/v2/Customizing-Git-Git-Configuration
+echo
+echo ---------------------------------------------
+echo Git required to add your full-name and e-mail.
+echo This will set-up the  --global user.name and --global user.email
+echo ---------------------------------------------
+echo
+read -p "Enter Your Full Name: " FullName
+read -p "Enter Your E-mail: " Email
+
+# Setting Up Git
+git config --global user.name "$FullName"
+git config --global user.email $Email
+
+# SmartGit
+# Downloading a GUI git client
+sudo wget http://www.syntevo.com/download/smartgithg/smartgithg-generic-6_0_7.tar.gz
+
+# Extracting
+sudo tar -zxvf smartgithg-generic-6_0_7.tar.gz
+sudo rm smartgithg-generic-6_0_7.tar.gz
+# Moving to /opt directory
+sudo mv smartgithg-6_0_7 /opt/
+# Creating a simbolic link so we an run this from the terminal
+sudo ln -s /opt/smartgithg-6_0_7/bin/smartgithg.sh /usr/bin/smartgit
+
+sudo touch /usr/share/applications/smartgit.desktop
+
+sudo echo "Name=SmartGit" >> /usr/share/applications/smartgit.desktop
+sudo echo "Comment=GUI git client" >> /usr/share/applications/smartgit.desktop
+sudo echo "Exec=/opt/smartgithg-6_0_7/bin/smartgithg.sh" >> /usr/share/applications/smartgit.desktop
+sudo echo "Icon=/opt/smartgithg-6_0_7/bin/smartgithg-128.png" >> /usr/share/applications/smartgit.desktop
+sudo echo "Type=Application" >> /usr/share/applications/smartgit.desktop
+sudo echo "StartupNotify=true" >> /usr/share/applications/smartgit.desktop
+sudo echo "Categories=GIT;Development;Utility;" >> /usr/share/applications/smartgit.desktop
+
+
 
 # Install Curl
 sudo apt-get update
@@ -74,6 +127,52 @@ ln -s /usr/share/drush/drush /usr/bin/drush
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
+
+# Installing Packages for Atom .
+# Drupal Packages:
+
+# language-drupal (http://drupal.org) syntax highlighting and snippets for Atom.
+sudo apm install language-drupal
+# drupal Drupal snippets, filetypes and hotkeys
+sudo apm install drupal
+# drupal-info-file Drupal info file syntax
+sudo apm install drupal-info-file
+
+# Atom Web Development
+# color-picker A Color Picker for the Atom Editor.
+sudo apm install color-picker
+
+#emmet The essential tool for web developers
+sudo apm install emmet
+
+#fancy-new-file Create files and directories by typing a relative path
+sudo apm install fancy-new-file
+
+# jshint Validate JavaScript with JSHint. In realtime or on save. Supports JSX
+sudo apm install jshint
+
+# atom-alignment A simple key-binding for aligning multi-line and multiple
+# selections in Atom (Based on the sublime text plugin)
+sudo apm install atom-alignment
+
+# change-case Change the case of selected text using node-change-case
+sudo apm install change-case
+
+# linter Validates your code using linters
+sudo apm install linter
+
+# markdown-preview Open a rendered version of the
+# Markdown in the current editor with `ctrl-shift-m'
+sudo apm install markdown-preview
+
+# atom-html-preview A live preview tool for Atom Editor.
+sudo apm install atom-html-preview
+
+# bootstrap-3-snippetset A 'Twitter Bootstrap 3' snippetset.
+sudo apm install bootstrap-3-snippetset
+
+# atom-beautifier Beautifier for Atom
+sudo apm install atom-beautifier
 
 # Install Nano (Terminal Editor)
 sudo apt-get update
