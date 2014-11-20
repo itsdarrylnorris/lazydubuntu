47b654f4f6b0abf338f6710030f386834234e6b5
Darryl Norris <admin@darrylnorris.com>
2014-11-20 15:29:48 -0600

    Developer Broswers
    This will fix #8 now this project will have Firefox Developer Browser and Google Chrome

--- lazydubuntu.sh
+++ lazydubuntu.sh
@@ -214,3 +214,15 @@ sudo echo "alias gc='git commit'" >> ~/.bash_aliases
 sudo echo "alias gd='git diff'" >> ~/.bash_aliases
 sudo echo "alias go='git checkout '" >> ~/.bash_aliases
 sudo echo "alias gcm='git commit -m'" >> ~/.bash_aliases
+
+# Installing Broswer Firefox for Developer & Google Chrome
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+# Firefox For Developers
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
+sudo apt-get update
+sudo apt-get install firefox
