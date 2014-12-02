670b1b20cef0d5095054b938da0ccfd45cab6528
Darryl Norris <admin@darrylnorris.com>
2014-12-02 15:44:49 -0600

    Update lampstack.sh
    Moving phpMyadmin to the lampstack.sh. In addition automation MySQL and phpMyAdmin installation for Apache2. The installation process as to set a password for MySQL and the password is 'password'.

--- bin/lampstack.sh
+++ bin/lampstack.sh
@@ -1,9 +1,16 @@
 # LampStack
-# This shell script is going to contain Apache MySQL and PHP5.
-
+# This shell script is going to contain Apache2 MySQL and PHP5.
 
 # Install LAMP Stack
 sudo apt-get update
+
+## Automating the MySQL installation 
+
+MYSQLPassword="password" # Setting up the password to be 'password'.
+
+echo mysql-server-5.5 mysql-server/root_password password $MYSQLPassword | debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password $MYSQLPassword | debconf-set-selections
+
 sudo apt-get install lamp-server^ -y
 
 # During the LAMP Stack instalation MySQL Server is going to ask
@@ -16,6 +23,21 @@ sudo a2enmod rewrite
 lineNumber="$( /bin/grep -n "/var/www/" /etc/apache2/apache2.conf | /usr/bin/head -1 | cut -d ":" -f 1 )"
 nextLineNumber="$( /usr/bin/tail -n +$lineNumber /etc/apache2/apache2.conf | /bin/grep -n "AllowOverride" | /usr/bin/head -1 | cut -d ":" -f 1 )"
 theExactLine="$( expr $lineNumber + $nextLineNumber - 1 )"/bin/sed -i -e "$theExactLine"s/None/All/ /etc/apache2/apache2.conf
+
+
+# This automate the Phpmyadmin installation. 
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password password' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password password' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password password' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+
+sudo apt-get install phpmyadmin -y 
+
+# This will fix the MyScript complain on PhpMyadmin.
+ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/cli/conf.d/20-mcrypt.ini
+ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/apache2/conf.d/20-mcrypt.ini
+php5enmod mcrypt
 
 # Restart server to after the is enable mod_rewrite and the apache.conf
 sudo service apache2 restart
