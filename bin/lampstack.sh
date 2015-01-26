c9e57174fae50101375a776e5d5326b6a9242af8
Darryl Norris <admin@darrylnorris.com>
2015-01-26 13:29:53 -0800

    Installing Xdebug

--- bin/lampstack.sh
+++ bin/lampstack.sh
@@ -11,7 +11,6 @@ echo mysql-server-5.5 mysql-server/root_password_again password $MYSQLPassword |
 
 sudo apt-get install lamp-server^ -y
 
-
 # Apache Persmission For Virtual Host
 sudo chmod -R 755 /var/www
 
@@ -36,6 +35,17 @@ php5enmod mcrypt
 # Restart server to after the is enable mod_rewrite and the apache.conf
 sudo service apache2 restart
 
+# Installing Xdebug
+sudo apt-get install php5-xdebug
+
+# Configure Xdebug
+sudo echo '' >> /etc/php5/apache2/php.ini 
+sudo echo '; Added for xdebug' >> /etc/php5/apache2/php.ini 
+sudo echo 'zend_extension="/usr/lib/php5/20121212/xdebug.so"' >> /etc/php5/apache2/php.ini 
+sudo echo 'xdebug.remote_enable=1' >> /etc/php5/apache2/php.ini 
+sudo echo 'xdebug.remote_handler=dbgp xdebug.remote_mode=req' >> /etc/php5/apache2/php.ini 
+sudo echo 'xdebug.remote_host=127.0.0.1 xdebug.remote_port=9000' >> /etc/php5/apache2/php.ini 
+sudo service apache2 restart
 
 # Virtual Host Support - Adding new virtual host file.
 sudo cp bin/includes/new-website /usr/local/bin/new-website
